d9f95882abf3f22eccb7a69cc81b4655c4393923
Mukona7 <mukona111@gmail.com>
2024-02-16 20:58:02 +0000

    python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,83 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <Python.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
- * print_python_list_info - Print some basic info about Python lists
- * @p: PyObject
- *
- * Return: Nothing
- */
-void print_python_list_info(PyObject *p)
+  * is_palindrome - Checks if a singly linked list is a palindrome
+  * @head: The head of the singly linked list
+  *
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+  */
+int is_palindrome(listint_t **head)
 {
-PyObject *item;
-PyListObject *list = (PyListObject *)p;
-int i, size, alloc;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev_slow = NULL;
+    listint_t *second_half = NULL;
+    int is_palindrome = 1;
 
-size = Py_SIZE(p);
-alloc = list->allocated;
-printf("[*] Size of the Python List = %d\n", size);
-printf("[*] Allocated = %d\n", alloc);
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-for (i = 0; i < size; i++)
+    // Find the middle of the list
+    while (fast && fast->next)
+    {
+        prev_slow = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Reverse the second half of the list
+    second_half = slow;
+    if (fast) // odd number of nodes
+        second_half = slow->next;
+    prev_slow->next = NULL; // end the first half
+
+    reverse_list(&second_half); // reverse the second half
+
+    // Compare first and second halves
+    while (*head && second_half)
+    {
+        if ((*head)->n != second_half->n)
+        {
+            is_palindrome = 0;
+            break;
+        }
+        *head = (*head)->next;
+        second_half = second_half->next;
+    }
+
+    // Restore the original list
+    reverse_list(&second_half); // reverse the second half again
+    if (fast) // odd number of nodes
+        prev_slow->next = slow;
+    else
+        prev_slow->next = reverse_list(&second_half);
+
+    return is_palindrome;
+}
+
+/**
+  * reverse_list - Reverses a linked list
+  * @head: The head of the linked list
+  *
+  * Return: The head of the reversed linked list
+  */
+listint_t *reverse_list(listint_t **head)
 {
-item =  PyList_GetItem(p, i);
-printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return *head;
 }
-}
+
