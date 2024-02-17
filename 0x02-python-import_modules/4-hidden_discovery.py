73635656c8f0411e3163bb64b035ff08384880b4
Mukona7 <mukona111@gmail.com>
2024-02-17 06:04:11 +0000

    k

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    sys.argv.pop(0)
-    total = 0
-    for number in sys.argv:
-        total += int(number)
-    print("{}".format(total))
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
