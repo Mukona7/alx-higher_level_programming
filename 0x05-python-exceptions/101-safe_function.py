037214fd00ef7ee26c8f5776bab943320597ef8c
Mukona7 <mukona111@gmail.com>
2024-02-28 16:37:05 +0000

    python

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-    result = 0
-
-    for i in range(1, 4):  # Extend the loop range to cover more scenarios
-        try:
-            if i > a:
-                raise ValueError(f"Value of i ({i}) is too large compared to a ({a})")
-
-            result += a ** b / i
-        except ValueError as ve:
-            print(f"Error: {ve}")
-            result = b + a
-            break
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            result = b + a
-            break
-
-    return result
-
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
